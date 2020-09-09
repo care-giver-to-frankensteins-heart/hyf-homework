51819a629695d726439f01d261adbfe7813ca531
ipoljupci@gmail.com <ipoljupci@gmail.com>
2020-09-09 13:51:15 +0200

    added js2/week2 HW

--- javascript/javascript2/week2/hyf-bay/main.js
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -1,46 +1,51 @@
 console.log("Script loaded");
 
 const products = getAvailableProducts();
+
 products.sort((a, b) => (a.price > b.price ? 1 : -1));
 
-function renderProducts(products) {
+let productsToDisplay = [...products];
+let textFilter = "";
+let priceFilter = "";
+
+function renderProducts() {
   const listOfProducts = document.createElement("ul");
+  listOfProducts.innerHTML = "";
   const main = document.querySelector("main");
   main.appendChild(listOfProducts);
   const ul = document.querySelector("ul");
-  products.forEach((product) => {
+  ul.innerHTML = "";
+  productsToDisplay = products.filter((product) => {
+    if (textFilter !== "") {
+      if (!product.name.toLowerCase().includes(textFilter)) {
+        return false;
+      }
+    }
+    if (priceFilter !== "") {
+      if (product.price > parseInt(priceFilter)) {
+        return false;
+      }
+    }
+    return true;
+  });
+  document.querySelector("ul").innerHTML = "";
+  productsToDisplay.forEach((product) => {
     const li = document.createElement("li");
     li.innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
     ul.appendChild(li);
   });
 }
 
-renderProducts(products);
 const userInput = document.getElementById("products-box");
-
 userInput.addEventListener("keyup", function () {
-  if (userInput.value !== "") {
-    const filterByName = products.filter((pro) =>
-      pro.name.toLowerCase().includes(userInput.value)
-    );
-    document.querySelector("ul").innerHTML = "";
-    renderProducts(filterByName);
-  } else {
-    document.querySelector("ul").innerHTML = "";
-    renderProducts(products);
-  }
+  textFilter = userInput.value;
+  renderProducts();
 });
 
 const userInput2 = document.getElementById("price-box");
 userInput2.addEventListener("keyup", function () {
-  if (userInput2.value !== "") {
-    const filterbyPrice = products.filter(
-      (pro) => pro.price <= parseInt(userInput2.value)
-    );
-    document.querySelector("ul").innerHTML = "";
-    renderProducts(filterbyPrice);
-  } else {
-    document.querySelector("ul").innerHTML = "";
-    renderProducts(products);
-  }
+  priceFilter = userInput2.value;
+  renderProducts();
 });
+
+renderProducts();
