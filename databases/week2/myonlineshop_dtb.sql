dcaf40959afacbfe9dcd331415c49a4a668a27be
ember blooming <ember_b_moss@embers-MacBook-Air.local>
2020-10-14 11:26:49 +0200

    added auto increment to primary key value

--- databases/week2/myonlineshop_dtb.sql
+++ databases/week2/myonlineshop_dtb.sql
@@ -1,10 +1,10 @@
 DROP DATABASE IF EXISTS  myonlineshop;
 CREATE DATABASE myonlineshop;
 USE myonlineshop;
-DROP TABLE IF EXISTS `Product`;
+
 DROP TABLE IF EXISTS `Product`;
 CREATE TABLE Product (
-    ProId int NOT NULL,
+    ProId int NOT NULL AUTO_INCREMENT,
     Name varchar(255) NOT NULL,
     Price int(255) NOT NULL,
     Size enum('x-small','small','x-large') DEFAULT NULL,
@@ -14,7 +14,7 @@ INSERT INTO myonlineshop.Product (ProId, Name, Price, Size) VALUES ('1', 'Shirt'
 INSERT INTO myonlineshop.Product (ProId, Name, Price, Size) VALUES ('2', 'Shoe', '200', 'x-small');
 DROP TABLE IF EXISTS `Customers`;
 CREATE TABLE Customers (
-    Customer_ID int NOT NULL,
+    Customer_ID int NOT NULL AUTO_INCREMENT,
     Name varchar(255) NOT NULL,
     Phone int(255) NOT NULL,
     City enum('Copenhagen','Odense','Alborg') DEFAULT NULL,
@@ -24,9 +24,9 @@ INSERT INTO myonlineshop.Customers (Customer_ID, Name, Phone, City) VALUES ('1',
 INSERT INTO myonlineshop.Customers (Customer_ID, Name, Phone, City) VALUES ('2', 'Mark', '947586326', 'Odense');
 DROP TABLE IF EXISTS `Orders`;
 CREATE TABLE Orders (
-	Orders_ID int NOT NULL,
+	Orders_ID int NOT NULL AUTO_INCREMENT,
     Customer_ID int NOT NULL,
-    Order_date varchar(255) NOT NULL,
+    Order_date DATETIME NOT NULL,
     status char(2) NOT NULL,
     PRIMARY KEY (`Orders_ID`)
 );
