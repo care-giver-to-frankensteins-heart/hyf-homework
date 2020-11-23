5f7e9e5b067fd1e341bdd82e89ee4646e5026821
ember blooming <ember_b_moss@embers-MacBook-Air.local>
2020-11-23 18:34:14 +0100

    on click function changed

--- react/week2/Fibonacci/src/FibonacciIncrement.js
+++ react/week2/Fibonacci/src/FibonacciIncrement.js
@@ -3,6 +3,7 @@ export function FibonacciIncrement(prop) {
   const increment = () => {
     prop.setFiboList((previous) => {
       const fibSequence = [...previous];
+      //on click renders and displays next 10 fibonacci numbers
       for (let i = previous.length; i < previous.length + 10; i++) {
         fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
       }
