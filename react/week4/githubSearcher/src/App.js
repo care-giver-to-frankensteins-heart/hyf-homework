0b2816ad2303b7a6e33888567cb07075da83f432
ember blooming <ember_b_moss@embers-MacBook-Air.local>
2020-12-07 12:27:42 +0100

    app finished

--- react/week4/githubSearcher/src/App.js
+++ react/week4/githubSearcher/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createContext } from "react";
+import { useDebounce } from "use-debounce";
+import "./App.css";
+import Header from "./components/Header";
+import SearchUsers from "./components/SearchUsers";
+import Loader from "./components/Loader";
+import UsersList from "./components/UsersList";
+
+export const SearchContext = createContext();
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [debouncedQuery] = useDebounce(query, 250);
+
+  const fetchUsers = async (query) => {
+    if (query !== "") {
+      const url = `https://api.github.com/search/users?q=${query}`;
+      setLoading(true);
+      const users = await fetch(url)
+        .then((data) => data.json())
+        .catch((error) => setError(error.message));
+      setUsers(users.items);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers(debouncedQuery);
+  }, [debouncedQuery]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchContext.Provider value={{ query, setQuery, users }}>
+        <Header />
+        <SearchUsers />
+        {users && (
+          <section>
+            {error && <div>{error}</div>}
+            {loading && !error ? <Loader /> : <UsersList />}
+          </section>
+        )}
+      </SearchContext.Provider>
     </div>
   );
 }
